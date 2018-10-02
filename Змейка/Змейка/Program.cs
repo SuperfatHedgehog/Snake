fe46c69fe0c9b8b29a1bf54bd87f6fb10257a810
SuperfatHedgehog <sarcasm7373@gmail.com>
2018-10-02 16:26:35 +0300

    9 уроков

---
+++ Змейка/Змейка/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Змейка
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p = new Point(4, 5, '*');
+
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
+
+            HLine UpLine = new HLine(0, 78, 0, '*');
+            UpLine.Draw();
+            HLine DownLine = new HLine(0, 78, 24, '*');
+            DownLine.Draw();
+            VLine LeftLine = new VLine(0, 0, 24, '*');
+            LeftLine.Draw();
+            VLine RightLine = new VLine(78, 0, 24, '*');
+            RightLine.Draw();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Змейка/Змейка/VLine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Змейка
+{
+    class VLine : Figure
+    {
+        public VLine(int x, int yUp, int yDown, char sym)
+        {
+            pList = new List<Point>();
+            for (int y = yUp; y <= yDown; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+    }
+}
